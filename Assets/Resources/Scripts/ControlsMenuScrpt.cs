99df2efd933c57b896ac0c1ea1240f3219ce0d37
flavius841 <flaviusepintilie@gmail.com>
2025-11-16 14:05:22 +0200

    Working on Controls Menu and reset function

--- Assets/Resources/Scripts/ControlsMenuScrpt.cs
+++ Assets/Resources/Scripts/ControlsMenuScrpt.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class ControlsMenuScrpt : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
-    }
 
+    [SerializeField] GameObject controlsMenu;
+    [SerializeField] bool active;
     
+        
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (!active)
+            {
+                controlsMenu.SetActive(true);
+                active = true;
+            }
+            else
+            {
+                controlsMenu.SetActive(false);
+                active = false;
+            }
+        }
     }
 }
---
+++ Assets/Resources/Scripts/ResetCar.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ResetCar : MonoBehaviour
+{
+
+    [SerializeField] GameObject Car;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.R))
+       {
+            Car.transform.position = new Vector3(0, -2, 0);
+            Car.transform.rotation = Quaternion.Euler(0, 0, 0);
+       }   
+    }
+}
